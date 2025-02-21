19075d019920a8df407e84938280adc3b0fb6a95
Alloutnoob <90827740+bryaburg@users.noreply.github.com>
2025-02-20 19:12:15 -0600

    Update

--- lua/config/plugins/Telescope.lua
+++ lua/config/plugins/Telescope.lua
@@ -5,7 +5,10 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { 
+      "nvim-lua/plenary.nvim",
+      "rcarriga/nvim-notify",
+    },
     config = function()
       require("telescope").setup({
         pickers = {
@@ -18,14 +21,20 @@ return {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
+          ["notify"] = {
+            -- You can add custom notify telescope config here
+          },
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep Files" })
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Recent Files" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+      vim.keymap.set("n", "<leader>fn", "<cmd>Telescope notify<cr>", { desc = "Search Notifications" })
 
       require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("notify")
     end,
   },
 }
--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -1,7 +1,7 @@
 return {
   "folke/snacks.nvim",
   dependencies = {
-    "echasnovski/mini.icons",
+    "echasnovski/mini.icons", -- we'll keep this as it's needed for icons
   },
   priority = 1000,
   lazy = false,
@@ -11,36 +11,36 @@ return {
       enabled = true,
       preset = {
         header = [[
-                                                                     
-       ████ ██████           █████      ██                     
-      ███████████             █████                             
-      █████████ ███████████████████ ███   ███████████   
-     █████████  ███    █████████████ █████ ██████████████   
-    █████████ ██████████ █████████ █████ █████ ████ █████   
-  ███████████ ███    ███ █████████ █████ █████ ████ █████  
- ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+                                                                     
+       ████ ██████           █████      ██                     
+      ███████████             █████                             
+      █████████ ███████████████████ ███   ███████████   
+     █████████  ███    █████████████ █████ ██████████████   
+    █████████ ██████████ █████████ █████ █████ ████ █████   
+  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+ ██████  █████████████████████ ████ █████ █████ ████ ██████ 
         ]],
       },
     },
     indent = { enabled = true },
     input = { enabled = true },
-    git = { enabled = false },
+    git = { enabled = true },
     picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
-    scroll = { enabled = false },
+    scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
   },
   keys = {
-    { "<leader>sf",       function() Snacks.scratch() end,            desc = "Toggle Scratch Buffer" },
-    { "<leader>S",        function() Snacks.scratch.select() end,     desc = "Select Scratch Buffer" },
-    { "<leader>gl",       function() Snacks.lazygit.log_file() end,   desc = "Lazygit Log (cwd)" },
-    { "<leader>lg",       function() Snacks.lazygit() end,            desc = "Lazygit" },
-    { "<C-p>",            function() Snacks.picker.pick("files") end, desc = "Find Files" },
-    { "<leader><leader>", function() Snacks.picker.recent() end,      desc = "Recent Files" },
-    { "<leader>fb",       function() Snacks.picker.buffers() end,     desc = "Buffers" },
-    { "<leader>fg",       function() Snacks.picker.grep() end,        desc = "Grep Files" },
-    { "<C-n>",            function() Snacks.explorer() end,           desc = "Explorer" },
+    { "<leader>sf", function() require("snacks").scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>S", function() require("snacks").scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>gl", function() require("snacks").lazygit.log_file() end, desc = "Lazygit Log (cwd)" },
+    { "<leader>lg", function() require("snacks").lazygit() end, desc = "Lazygit" },
+    { "<C-p>", function() require("snacks").picker.pick("files") end, desc = "Find Files" },
+    { "<leader><leader>", function() require("snacks").picker.recent() end, desc = "Recent Files" },
+    { "<leader>fb", function() require("snacks").picker.buffers() end, desc = "Buffers" },
+    { "<leader>fg", function() require("snacks").picker.grep() end, desc = "Grep Files" },
+    { "<C-n>", function() require("snacks").explorer() end, desc = "Explorer" },
   }
 }

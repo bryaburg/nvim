29bdca8dec0ed9a804e04ce660ddd5a8ae8bafc1
Alloutnoob <bryanburgess95@hotmail.com>
2025-08-01 19:22:08 -0500

    update

--- lua/alloutnoob/plugins/lsp.lua
+++ lua/alloutnoob/plugins/lsp.lua
@@ -1,43 +1,102 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      'saghen/blink.cmp',
-      {
-        "folke/lazydev.nvim",
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
+  local lsp = require("lsp-zero")
+
+  lsp.preset("recommended")
+
+  lsp.ensure_installed({
+    "tsserver",
+    "rust_analyzer",
+    "asm-lsp",
+    "clangd",
+    "lua-language-server",
+    "bash-language-server",
+    "tailwindcss-language-server",
+    "docker-compose-language-server",
+    "json-lsp",
+    "html-lsp",
+    "eslint-lsp",
+    "markdown-oxide",
+    "gopls",
+    "pyright",
+    "sqls",
+    "yaml-language-server",
+  })
+
+  -- Fix Undefined global 'vim'
+  lsp.configure("lua-language-server", {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
         },
       },
     },
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      require("lspconfig").lua_ls.setup { capabilites = capabilities }
+  })
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local c = vim.lsp.get_client_by_id(args.data.client_id)
-          if not c then return end
+  local cmp = require("cmp")
+  local cmp_select = { behavior = cmp.SelectBehavior.Select }
+  local cmp_mappings = lsp.defaults.cmp_mappings({
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+  })
 
-          if vim.bo.filetype == "lua" then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
-              end,
-            })
-          end
-        end,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require'lspconfig'.lua_ls.setup {}
-    end
-  }
+  cmp_mappings["<Tab>"] = nil
+  cmp_mappings["<S-Tab>"] = nil
+
+  lsp.setup_nvim_cmp({
+    mapping = cmp_mappings,
+  })
+
+  lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+      error = "E",
+      warn = "W",
+      hint = "H",
+      info = "I",
+    },
+  })
+
+  lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gd", function()
+      vim.lsp.buf.definition()
+    end, opts)
+    vim.keymap.set("n", "K", function()
+      vim.lsp.buf.hover()
+    end, opts)
+    vim.keymap.set("n", "<leader>vws", function()
+      vim.lsp.buf.workspace_symbol()
+    end, opts)
+    vim.keymap.set("n", "<leader>vd", function()
+      vim.diagnostic.open_float()
+    end, opts)
+    vim.keymap.set("n", "[d", function()
+      vim.diagnostic.goto_next()
+    end, opts)
+    vim.keymap.set("n", "]d", function()
+      vim.diagnostic.goto_prev()
+    end, opts)
+    vim.keymap.set("n", "<leader>vca", function()
+      vim.lsp.buf.code_action()
+    end, opts)
+    vim.keymap.set("n", "<leader>vrr", function()
+      vim.lsp.buf.references()
+    end, opts)
+    vim.keymap.set("n", "<leader>vrn", function()
+      vim.lsp.buf.rename()
+    end, opts)
+    vim.keymap.set("i", "<C-h>", function()
+      vim.lsp.buf.signature_help()
+    end, opts)
+  end)
+
+  lsp.setup()
+
+  vim.diagnostic.config({
+    virtual_text = true,
+  })
 }

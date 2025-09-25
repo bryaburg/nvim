759971a2ba53dfec2a164b5120572895ca05fa2b
Alloutnoob <bryanburgess95@hotmail.com>
2025-09-25 11:45:01 -0500

    update

---
+++ lua/alloutnoob/plugins/blink.lua
@@ -0,0 +1,24 @@
+return
+{
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  version = '1.*',
+
+  opts = {
+    keymap = { preset = 'default' },
+
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono'
+    },
+
+    signature = {
+      enabled = true,
+      timeout = 4000,
+      auto_dismiss = true,
+      floating_window = true,
+      hint = 'Press <Tab> to accept',
+    },
+  },
+}
